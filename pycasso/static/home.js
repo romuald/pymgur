54abfc3c1f18153fa0b13aad275a8fbff53a1be3
Romuald Brunet <romuald@chivil.com>
2017-12-17 18:45:09 +0100

    Add loader on thumbnails

--- pycasso/static/home.js
+++ pycasso/static/home.js
@@ -1,5 +1,15 @@
 function HomeLoaded() {
+	var imgs = document.querySelectorAll('#latest img');
 
+	var loader = document.querySelector('#loader');
+	var loaded = 0;
+
+	for (var i = 0; i < imgs.length; i++) {
+		// Cannot seem to find a DOM event for missing image
+		imgs[i].onload = imgs[i].onerror = function() {
+			loader.style.width = ((++loaded / imgs.length) * 100) + '%';
+		};
+	}
 }
 
 if ( document.readyState === "loading" ) {
