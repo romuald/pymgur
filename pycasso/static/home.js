8834425731fad1b4aa6ec06faf24689ca00c0223
Romuald Brunet <romuald@chivil.com>
2017-12-17 23:34:24 +0100

    Much better upload style

--- pycasso/static/home.js
+++ pycasso/static/home.js
@@ -1,32 +1,33 @@
 function HomeLoaded() {
+	var i;
 	var imgs = document.querySelectorAll('#latest img');
 
 	var loader = document.querySelector('#loader');
 	var loaded = 0;
 
-	for (var i = 0; i < imgs.length; i++) {
+	for (i = 0; i < imgs.length; i++) {
 		// Cannot seem to find a DOM event for missing image
 		imgs[i].onload = imgs[i].onerror = function() {
 			loader.style.width = ((++loaded / imgs.length) * 100) + '%';
 		};
 	}
-	var upicon =  document.querySelector("#imageup");
-	var upinput = document.querySelector("#image-up");
 
-	upinput.addEventListener("change", function() {
+	function inputChange(ev) {
 		var reader = new FileReader();
+		var preview = this.parentNode.querySelector(".upload-preview");
 		reader.onload = function() {
-			upicon.setAttribute("style", "background-image: url(" +  this.result + ")");
+			// XXX check state just in case
+			preview.style.backgroundImage = "url(" + this.result + ")";
 		}
 		reader.readAsDataURL(this.files[0]);
-	})
+	}
 
-	
-	upicon.addEventListener("click", function(ev) {
-		ev.preventDefault();
-		upinput.click();
-	})
-	upinput.parentNode.style.display = "none";
+	var uprows = document.querySelectorAll(".imagerow");
+	for (i = 0; i < uprows.length; i++) {
+		var input = uprows[i].querySelector('input[type="file"]');
+		input.addEventListener("change", inputChange);
+		input.style.display = "none";
+	}
 }
 
 if ( document.readyState === "loading" ) {
