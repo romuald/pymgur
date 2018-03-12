85173e45a61e6e27611767f4a3537626fa60ce16
Romuald Brunet <romuald@chivil.com>
2018-03-12 22:48:34 +0100

    Externalize configuration

--- pymgur/__init__.py
+++ pymgur/__init__.py
@@ -1,40 +1,87 @@
 import os
+import os.path
 import re
+import ast
 import random
+import argparse
+import configparser
+
 from pprint import pprint
 from datetime import datetime, timedelta
 from contextlib import closing
 import sqlite3
 
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-render_template, flash
+    render_template, flash
 from werkzeug.contrib.fixers import ProxyFix
 
 
 app = Flask(__name__)
-app.config.update({
-    'DATADIR': os.path.join(app.root_path, 'data'),
-    # 'SECRET_KEY': 'plokiploki',  # should not be used (yet)
-    'TEMPLATES_AUTO_RELOAD': True,
-    'MAX_CONTENT_LENGTH': 48 * 1024 * 1024,
-    'PREVIEW_SIZE': 800,  # in pixels
-    'THUMBNAIL_SIZE':  200,  # ditto
-    'DEFAULT_PRIVATE': False,
-    'DEFAULT_TTL': '7D', # YMDhms, cumulative (eg: 1M15D -> 35 days)
-    'MAX_TTL': None,  # ditto, may be None for no maximum
-    'MAX_IMAGES': 8,  # maximum number of images in an imageset
-    'TTLS': ['10s', '1h', '3h', '6h', '1D', '3D', '7D', '1M', '3M', '6M', '-'],
-    'PROXIES': 1,
-    'JPEG_QUALITY': 90,
-})
 
-if app.config['PROXIES'] > 0:
-    app.wsgi_app = ProxyFix(app.wsgi_app, app.config['PROXIES'])
 
-# app.config.from_envvar('PYCASSO_SETTINGS', silent=True)
+def boolean(value):
+    if value.lower() in ('true', 't', 'yes', 'y'):
+        return True
+
+    if value.lower() in ('false', 'f', 'no', 'n'):
+        return False
+
+    raise ValueError('Invalid boolean value: %r' % value)
+
+
+def config_value(key, value):
+    if value == '':
+        return None
+
+    return {'TEMPLATES_AUTO_RELOAD': boolean,
+            'MAX_CONTENT_LENGTH': int,
+            'PREVIEW_SIZE': int,
+            'THUMBNAIL_SIZE': int,
+            'DEFAULT_PRIVATE': boolean,
+            'MAX_IMAGES': int,
+            'PROXIES': int,
+            'JPEG_QUALITY': int,
+        }.get(key.upper(), str)(value)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='TBD')
+
+    parser.add_argument('--config', '-c', type=argparse.FileType('r'))
+
+    return parser.parse_args()
+
+
+def configure():
+    args = parse_args()
+
+    mydir = os.path.dirname(__file__)
+
+    cparser = configparser.ConfigParser()
+    cparser.read(os.path.join(mydir, 'pymgur.default.ini'))
+
+    if args.config:
+        cparser.read_string(args.config.read())
+
+    section = cparser['pymgur']
+    section.update({
+        'app_root_path': mydir,
+        'run_path': os.getcwd()
+    })
+    values = {k.upper(): config_value(k, v) for k, v in section.items()}
+
+    # TTLS is a list
+    values['TTLS'] = map(str.strip, values['TTLS'].split(','))
+    app.config.update(values)
+
+
 from . import datastore
 from . import views
 
 def main():
     """Dev runner"""
+    configure()
+
+    if app.config['PROXIES'] > 0:
+        app.wsgi_app = ProxyFix(app.wsgi_app, app.config['PROXIES'])
     app.run(use_reloader=True, threaded=True)
