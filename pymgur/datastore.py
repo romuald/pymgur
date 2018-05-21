0407c99002899a671032677c94f338198e085a16
Romuald Brunet <romuald@chivil.com>
2018-05-21 09:30:25 +0200

    future todo for init db

--- pymgur/datastore.py
+++ pymgur/datastore.py
@@ -49,6 +49,7 @@ def initdb_command():
 
 def init_db():
     """Initializes the database."""
+    # XXX Use locking mechanism to handle concurrent calls
     db = get_db()
     with app.open_resource('schema.sql', mode='r') as f:
         db.cursor().executescript(f.read())
